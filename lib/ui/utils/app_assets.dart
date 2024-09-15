661301bba6bd2d0f99f942cd6d5388e62aa0dfe9
EbrahimTarek02 <ebrahimtarek2002@gmail.com>
2024-09-15 14:39:50 +0300

    Initial Commit.

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+
+  static const String imagesPath = "assets/images/";
+
+  static const String appLogo = "${imagesPath}app_logo.png";
+  static const String wishListIcon = "${imagesPath}wish_list_icon.png";
+  static const String searchIcon = "${imagesPath}search_icon.png";
+  static const String cartIcon = "${imagesPath}cart_icon.png";
+  static const String plusIcon = "${imagesPath}plus_icon.png";
+  static const String emptyListImage = "${imagesPath}empty_list_image.jpeg";
+}
---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0XFF004182);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color yellow = Color(0XFFFDD835);
+  static const Color red = Color(0XFFF44336);
+}
